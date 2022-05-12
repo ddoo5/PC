3d9c8e356267d04fbfeac4218ac9bc71f5825f22
l_md_l <maks.degtearyov25@gmail.com>
2022-05-12 15:06:37 +0300

    start pc_code

---
+++ PC_code/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+List<char> lowlet = new List<char>();
+lowlet.AddRange("abcdefghijklmnopqrstuvwxyz");
+List<char> caplet = new List<char>();
+caplet.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+List<char> sign = new List<char>();
+sign.AddRange("-'^*~@/!#$%&");
