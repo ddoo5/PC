78934442c3a07b347d1db531fa09f6eb8f7338dc
D <kritantablake@gmail.com>
2022-06-02 19:58:47 +0300

    add hentai and update program.cs

--- PC_code/Program.cs
+++ PC_code/Program.cs
@@ -1,7 +1,23 @@
 ﻿using System.Collections.Generic;
-List<char> lowlet = new List<char>();
-lowlet.AddRange("abcdefghijklmnopqrstuvwxyz");
-List<char> caplet = new List<char>();
-caplet.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-List<char> sign = new List<char>();
-sign.AddRange("-'^*~@/!#$%&");
+using PC_code;
+
+Draw();
+
+List<char> _lowlet = new List<char>();
+_lowlet.AddRange("abcdefghijklmnopqrstuvwxyz");
+
+List<char> _caplet = new List<char>();
+_caplet.AddRange("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+List<char> _sign = new List<char>();
+_sign.AddRange("-'^*~@/!#$%&");
+
+List<char> _numbers = new List<char>();
+_numbers.AddRange("1234567890");
+
+void Draw()
+{
+    //Console.SetWindowSize(1000, 1000);
+    Hentai.PictureOne();
+    Hentai.PictureTwo();
+}
