5ca140adcac530828f2d1172c0cced89f6390522
l_md_l <maks.degtearyov25@gmail.ru>
2022-06-05 14:13:41 +0300

    edit Program.cs

--- PC_code/Program.cs
+++ PC_code/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using PC_code;
 
-Hentai.Picture();
-
 List<char> _lowlet = new List<char>();
 _lowlet.AddRange("abcdefghijklmnopqrstuvwxyz");
 
@@ -15,41 +13,101 @@ _sign.AddRange("-'^*~@/!#$%&");
 List<char> _numbers = new List<char>();
 _numbers.AddRange("1234567890");
 
-Random rnd = new Random();
+Random rnd = new();
 int x;
 
-char lowlet()
+
+Hentai.Picture(); //вывод картинки
+
+
+while (true)   //основной метод
+{
+    try
+    {
+        Console.WriteLine("Выберите нужный вид пароля, написав цифру\n" +
+        "1) обычный пароль(6 символов)\n" +
+        "2) пароль с использованием цифр(8 символов)\n" +
+        "3) пароль с использованием спец.символов(8 символов)\n" +
+        "4) сложный пароль(16 символов)");
+
+        int typen = Convert.ToInt32(Console.ReadLine());
+        string password;
+
+        switch (typen)
+        {
+            case 1:
+                password = Creator(1, 6);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(password);
+                Console.ResetColor();
+                break;
+            case 2:
+                password = Creator(2, 8);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(password);
+                Console.ResetColor();
+                break;
+            case 3:
+                password = Creator(3, 8);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(password);
+                Console.ResetColor();
+                break;
+            case 4:
+                password = Creator(3, 16);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(password);
+                Console.ResetColor();
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введите нужную цифру");
+                Console.ResetColor();
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Exception: {ex.Message}");
+        Console.ResetColor();
+    }
+}
+
+
+char lowlet() // Метод для выбора рандомной строчной буквы
 {
     x = rnd.Next(0, _lowlet.Count);
     char c = _lowlet[x];
     return c;
 }
 
-char caplet()
+char caplet()  // Метод для выбора рандомной прописной буквы
 {
     x = rnd.Next(0, _caplet.Count);
     char c = _caplet[x];
     return c;
 }
 
-char signs()
+char signs()  //Метод для выбора рандомного знака
 {
     x = rnd.Next(0, _sign.Count);
     char c = _sign[x];
     return c;
 }
 
-char numbers()
+char numbers()  //Метод для выбора рандомной цифры
 {
     x = rnd.Next(0, _numbers.Count);
     char c = _numbers[x];
     return c;
 }
 
-char swt(int q)
+char swt(int q)  //Метод, который рандомно выбирает тип символа для пароля
 {
     char letter = 'o';
     int r = rnd.Next(q + 1);
+
     switch (r)
     {
         case 0:
@@ -68,15 +126,16 @@ char swt(int q)
     return letter;
 }
 
-string _rnd(int n, int y)
+string Creator(int n, int y)  //Метод, который создает пароль, в зависимости от нужного типа
 {
-    string word = "";
+    string password = "";
+
     if (n == 1)
     {
         for (int i = 0; i < y; i++)
         {
             char f = swt(1);
-            word += f;
+            password += f;
         }
     }
     else if (n == 2)
@@ -84,7 +143,7 @@ string _rnd(int n, int y)
         for (int i = 0; i < y; i++)
         {
             char f = swt(2);
-            word += f;
+            password += f;
         }
     }
     else
@@ -92,48 +151,8 @@ string _rnd(int n, int y)
         for (int i = 0; i < y; i++)
         {
             char f = swt(3);
-            word += f;
+            password += f;
         }
     }
-    return word;
-}
-
-Console.WriteLine("Выберите нужный вид пароля, написав цифру\n1) обычный пароль(6 символов)\n" +
-    "2) пароль с использованием цифр(8 символов)\n" +
-    "3) пароль с использованием спец.символов(8 символов)\n4) сложный пароль(16 символов)");
-
-try
-{
-    int typen = Convert.ToInt32(Console.ReadLine());
-    string password;
-    switch (typen)
-    {
-        case 1:
-            password = _rnd(1, 6);
-            Console.WriteLine(password);
-            break;
-        case 2:
-            password = _rnd(2, 8);
-            Console.WriteLine(password);
-            break;
-        case 3:
-            password = _rnd(3, 8);
-            Console.WriteLine(password);
-            break;
-        case 4:
-            password = _rnd(3, 16);
-            Console.WriteLine(password);
-            break;
-        default:
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Введите нужную цифру");
-            break;
-
-
-    }
-}
-catch (Exception)
-{
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine("Введите нужную цифру");
+    return password;
 }
